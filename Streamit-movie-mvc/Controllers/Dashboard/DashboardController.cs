9e4dbd93e9043fc1f705a8c5d71bb8ba93b5e4bc
huy trần <huy110903@gmail.com>
2024-02-20 14:27:35 +0700

    Add project files.

---
+++ Streamit-movie-mvc/Controllers/Dashboard/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Streamit_movie_mvc.Controllers.Dashboard
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
